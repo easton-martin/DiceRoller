bc26169dbeb8bb1cf824cbdde916b9e46df16207
easton-martin <easton.martin@usu.edu>
2025-10-28 18:47:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+// Name: Easton Martin
+// Description: Simulate rolling dice
 
+function toggleDieHold(id){
+    // grab the data-active attribute and toggle between true and false
+    let currentActiveStatus = document.getElementById(id).getAttribute("data-active");
+    let newStatus = currentActiveStatus==="true"?false:true;
+    document.getElementById(id).setAttribute("data-active", newStatus);
+    // apply the held class accordingly
+    if(newStatus){
+        document.getElementById(id).classList.remove("held")
+    }
+    else{
+        document.getElementById(id).classList.add("held")
+    }
+}
 
+function rollDice(){
+    // grab all active dice
+    let allActiveDice = document.querySelectorAll('[data-active="true"]');
+    // roll the dice and update src and alt
+    for(let die of allActiveDice){
+        let num = Math.floor(Math.random()*6)+1
+        die.setAttribute("src", `images/${num}.png`);
+        die.setAttribute("alt", `Die ${num}`);
+    }
+}
